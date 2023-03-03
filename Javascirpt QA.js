09f00c113fa3c713211b3ab378991e354d91e08c
Mbilal6799 <ftk4321@gmail.com>
2023-03-03 15:14:20 -0800

    firstCommit

---
+++ Javascirpt QA.js
@@ -0,0 +1,71 @@
+
+
+// 1. What is the output of the following code?
+// const a={a:'a'};
+// const a={a:'b'};
+
+// 2. What is the output of the following code?
+// let a={a:'a'};
+// let a={a:'a'};
+
+// 3. What is the output of the following code?
+// let a={a:'a'};
+// a={a:'a'};
+
+// 4. What is the output of the following code?
+// const a={a:'a'};
+// a={b:'b'};
+
+// 5. What is the output of the following code?
+// var a={a:'a'};
+// var a={a:'b'};
+
+// 6. What is the output of the following code?
+// var a={a:'a'};
+// var a={a:'b'};
+
+// 7. What is the output of the following code?
+//How to add a property to an object on First and Last index?
+// const a=[1,2,3];
+
+// 9. How to convert array Value in String Like This a=['1','2','3']?
+// const a=[1,2,3];
+
+// 10. Is Javascript is single threaded or multi threaded?
+
+// 11. What is the output of the following code?
+// function a(){                        //function scope
+//     a1=10;
+//     var b1=10;
+//     if(a1=>10){                      //block scope
+//         var c1=11;
+//     }
+//     console.log(c1);
+// }
+//      a();
+//     console.log(a1);
+
+// 12. how to call innerfunction f1?
+// function a1(){
+//     return function f1(){
+//       console.log(1);
+//     }
+//   }
+
+// 13. how to switch value a and b? a=1,b=2?
+// ans in other file
+
+// 14. What is lexical scope?
+// if any variable is not defined in current scope then it will check in parent scope and so on.
+// example
+//let data=10;        // this variable is in global scope      
+//function a(){
+    //let a1=11;          // this variable is in function scope on use in a1() function
+    //console.log(data);
+//}
+//a();
+    //console.log(a1);   // this will give error because a1 is not defined in global scope
+
+
+
+
